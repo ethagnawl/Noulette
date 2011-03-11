b8901cd290bb79c45e6a51cf7b591f51da6952b6
pete <pete@pete-laptop.(none)>
2011-03-11 00:41:42 -0500

    moving update_players() templating server-side

--- noulette.js
+++ noulette.js
@@ -1,17 +1,218 @@
-var http = require('http')
+var html_template = "\
+<html>\
+<head>\
+    <title>{{title}}</title>\
+	<meta charset='UTF-8'>\
+    <link rel='stylesheet' type='text/css' href='/pub/css/noulette.css' media='all' />\
+    <script src='/pub/js/head.js'></script>\
+</head>\
+<body class='hide'>\
+    <h1>noulette</h1>\
+    <div id='column_left' class='fl-left'>\
+        <div id='flash'></div>\
+        <ul id='players'></ul>\
+        <button id='buy_more_chips'>buy more chips</button>\
+        <ul id='chips'>\
+            <li>Chip Stack</li>\
+        </ul>\
+        <ul id='results'>\
+            <li>Results</li>\
+        </ul>\
+    </div>\
+    <div id='column_right' class='fl-left'>\
+        <div id='layout'>\
+            <div id='wheel'></div>\
+            <table id='main_table'>\
+                <tr>\
+	                <td id='0' class='number'>0</td>\
+	                <td id='00' class='number'>00</td>\
+                </tr>\
+                <tr>\
+	                <td id='1' class='red number'>1</td>\
+	                <td id='2' class='black number'>2</td>\
+	                <td id='3' class='red number'>3</td>\
+                </tr>\
+                <tr>\
+	                <td id='4' class='black number'>4</td>\
+	                <td id='5' class='red number'>5</td>\
+	                <td id='6' class='black number'>6</td>\
+                </tr>\
+                <tr>\
+	                <td id='7' class='red number'>7</td>\
+	                <td id='8' class='black number'>8</td>\
+	                <td id='9' class='red number'>9</td>\
+                </tr>\
+                <tr>\
+	                <td id='10' class='black number'>10</td>\
+	                <td id='11' class='black number'>11</td>\
+	                <td id='12' class='red number'>12</td>\
+                </tr>\
+                <tr>\
+	                <td id='13' class='black number'>13</td>\
+	                <td id='14' class='red number'>14</td>\
+	                <td id='15' class='black number'>15</td>\
+                </tr>\
+                <tr>\
+	                <td id='16' class='red number'>16</td>\
+	                <td id='17' class='black number'>17</td>\
+	                <td id='18' class='red number'>18</td>\
+                </tr>\
+                <tr>\
+	                <td id='19' class='black number'>19</td>\
+	                <td id='20' class='black number'>20</td>\
+	                <td id='21' class='red number'>21</td>\
+                </tr>\
+                <tr>\
+	                <td id='22' class='black number'>22</td>\
+	                <td id='23' class='red number'>23</td>\
+	                <td id='24' class='black number'>24</td>\
+                </tr>\
+                <tr>\
+	                <td id='25' class='red number'>25</td>\
+	                <td id='26' class='black number'>26</td>\
+	                <td id='27' class='red number'>27</td>\
+                </tr>\
+                <tr>\
+	                <td id='28' class='red number'>28</td>\
+	                <td id='29' class='black number'>29</td>\
+	                <td id='30' class='red number'>30</td>\
+                </tr>\
+                <tr>\
+	                <td id='31' class='black number'>31</td>\
+	                <td id='32' class='red number'>32</td>\
+	                <td id='33' class='black number'>33</td>\
+                </tr>\
+                <tr>\
+	                <td id='34' class='red number'>34</td>\
+	                <td id='35' class='black number'>35</td>\
+	                <td id='36' class='red number'>36</td>\
+                </tr>\
+                <tr>\
+	                <td id='column_1'>2-1</td>\
+	                <td id='column_2'>2-1</td>\
+	                <td id='column_3'>2-1</td>\
+                </tr>\
+            </table>\
+            <table>\
+	            <tr>\
+	                <td id='1_12' class='third'>1st 12</td>\
+                    <td id='13_24' class='third'>2nd 12</td>\
+                    <td id='25_36' class='third'>3rd 12</td>\
+	            </tr>\
+            </table>\
+            <table>\
+	            <tr>\
+                    <td id='1_18' class='half'>1-18</td>\
+                    <td class='parity' id='even'>Even</td>\
+                    <td id='red' class='red color'>Red</td>\
+                    <td id='black' class='black color'>Black</td>\
+                    <td class='parity' id='odd'>Odd</td>\
+                    <td id='19_36' class='half'>19-36</td>\
+	            </tr>\
+            </table>\
+        </div>\
+    </div>\
+    <script>\
+    head.js('https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js', 'http://cdn.socket.io/stable/socket.io.js', '/pub/js/mustache.js', '/pub/js/noulette_client.js');\
+    </script>\
+</body>\
+</html>\
+";
+
+var sys = require('sys')
     ,   fs = require('fs')
-;
-    
-http.createServer(function (req, res) {
-    if (req.url === '/' || req.url == '/index.html') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(fs.readFileSync('index.html')); 
-    } else if (req.url == "/noulette.css") {
-console.log(req);
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(fs.readFileSync('noulette.css')); 
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end("Page Could Not Be Found"); 
+    ,   filename = process.argv[2]
+    ,   Url = require('url')
+    ,   http = require('http')
+    ,   querystring = require('querystring')
+    ,   static = require('node-static')
+    ,   app = require('express').createServer()
+    ,   socket = require('socket.io').listen(app)
+    ,   file = new(static.Server)('./pub')
+    ,   Mustache = require('mustache')
+    ,   view = {
+        title: "Noulette"
     }
-}).listen(5073, "127.0.0.1");
+    ,   players_arr = [] // use _ to build this from players{}
+    ,   players = {}
+    ,   layout_config = JSON.parse(fs.readFileSync(filename, 'utf8'))
+;
+
+function vithout(arr) {
+    var args = Array.prototype.slice.call(arguments).slice(1, arguments.length), arg, j, l, ll;
+        for (arg = 0, l = args.length; arg < l; arg += 1) {
+            for (j = 0, ll = arr.length; j < ll; j += 1) {
+                if (arr[j] === args[arg]) {
+                    arr.splice(j, 1); 
+                    j -= 1;
+                }
+            }
+        }
+    return arr;
+}
+
+function rando(arr) {
+    return Math.floor(Math.random() * arr.length);
+}
+
+function spin() {
+    var result = layout_config.numbers.nums[rando(layout_config.numbers.nums)]
+        ,   results = {
+            number: result.toString()
+            ,   color: layout_config.numbers[result]['color']
+            ,   parity: layout_config.numbers[result]['parity']
+        }
+    ;
+    return results;
+}
+
+var results;
+setInterval(function () {
+    results = spin();
+console.log(results);    
+    socket.broadcast({
+        spin: results
+    });
+}, 10000);
+
+function update_players_list() {
+    var view = {
+        players: players_arr 
+    }
+        ,   template = '{{#players}}<li>{{.}}</li>{{/players}}'
+        ,   players_partial = Mustache.to_html(template, view)
+    ;
+
+    socket.broadcast({
+        'players_arr': players_partial
+    });        
+}
+
+socket.on('connection', function (client) {
+    client.on('disconnect', function () {
+        vithout(players_arr, players[client.sessionId]['user_name']);
+        players[client.sessionId]['active'] = false;
+        update_players_list();
+    }).on('message', function (msg) {
+        if (msg.user_name) {
+            players_arr[players_arr.length] = msg.user_name;    // gheeeett0
+            players[client.sessionId] = {
+                user_name: msg.user_name,
+                active: true,
+                chips: 5
+            };
+            update_players_list();
+        }
+    });
+});
+
+app.get('/*.(js|css|png)', function(req, res) {
+    res.sendfile("/var/www/roulette" + req.url);
+}).get('/', function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(Mustache.to_html(html_template, view));
+});
+
+app.listen(8000);
+
+
---
+++ pub/js/noulette_client.js
@@ -0,0 +1,415 @@
+(function () {
+
+    var layout_config = {
+        "columns": {
+            "column_1": [
+                0,
+                1,
+                4,
+                7,
+                10,
+                13,
+                16,
+                19,
+                22,
+                25,
+                28,
+                31,
+                34
+            ],
+            "column_2": [
+                0,
+                "00",
+                2,
+                5,
+                8,
+                11,
+                14,
+                17,
+                20,
+                23,
+                26,
+                29,
+                32,
+                35
+            ],
+            "column_3": [
+                "00",
+                3,
+                6,
+                9,
+                12,
+                15,
+                18,
+                21,
+                24,
+                27,
+                30,
+                33,
+                36
+            ] 
+        },
+        "numbers": {
+            "nums": [
+                0,
+                28,
+                9,
+                26,
+                30,
+                11,
+                7,
+                20,
+                32,
+                17,
+                5,
+                22,
+                34,
+                15,
+                3,
+                24,
+                36,
+                13,
+                1,
+                "00",
+                27,
+                10,
+                25,
+                29,
+                12,
+                8,
+                19,
+                31,
+                18,
+                6,
+                21,
+                33,
+                16,
+                4,
+                23,
+                35,
+                14,
+                2
+            ],
+            "0": {
+                "color": "green",
+                "parity": "even" 
+            },
+            "28": {
+                "color": "red",
+                "parity": "even" 
+            },
+            "9": {
+                "color": "red",
+                "parity": "odd" 
+            },
+            "26": {
+                "color": "black",
+                "parity": "even" 
+            },
+            "30": {
+                "color": "red",
+                "parity": "even" 
+            },
+            "11": {
+                "color": "black",
+                "parity": "odd" 
+            },
+            "7": {
+                "color": "red",
+                "parity": "odd" 
+            },
+            "20": {
+                "color": "black",
+                "parity": "even" 
+            },
+            "32": {
+                "color": "red",
+                "parity": "even" 
+            },
+            "17": {
+                "color": "black",
+                "parity": "odd" 
+            },
+            "5": {
+                "color": "red",
+                "parity": "odd" 
+            },
+            "22": {
+                "color": "black",
+                "parity": "even" 
+            },
+            "34": {
+                "color": "red",
+                "parity": "even" 
+            },
+            "15": {
+                "color": "black",
+                "parity": "odd" 
+            },
+            "3": {
+                "color": "red",
+                "parity": "odd" 
+            },
+            "24": {
+                "color": "black",
+                "parity": "even" 
+            },
+            "36": {
+                "color": "red",
+                "parity": "even" 
+            },
+            "13": {
+                "color": "black",
+                "parity": "odd" 
+            },
+            "1": {
+                "color": "red",
+                "parity": "odd" 
+            },
+            "00": {
+                "color": "green",
+                "parity": "even" 
+            },
+            "27": {
+                "color": "red",
+                "parity": "odd" 
+            },
+            "10": {
+                "color": "black",
+                "parity": "even" 
+            },
+            "25": {
+                "color": "red",
+                "parity": "odd" 
+            },
+            "29": {
+                "color": "black",
+                "parity": "odd" 
+            },
+            "12": {
+                "color": "red",
+                "parity": "even" 
+            },
+            "8": {
+                "color": "black",
+                "parity": "even" 
+            },
+            "19": {
+                "color": "black",
+                "parity": "odd" 
+            },
+            "31": {
+                "color": "black",
+                "parity": "odd" 
+            },
+            "18": {
+                "color": "red",
+                "parity": "even" 
+            },
+            "6": {
+                "color": "black",
+                "parity": "even" 
+            },
+            "21": {
+                "color": "red",
+                "parity": "odd" 
+            },
+            "33": {
+                "color": "black",
+                "parity": "odd" 
+            },
+            "16": {
+                "color": "red",
+                "parity": "even" 
+            },
+            "4": {
+                "color": "black",
+                "parity": "even" 
+            },
+            "23": {
+                "color": "red",
+                "parity": "odd" 
+            },
+            "35": {
+                "color": "black",
+                "parity": "odd" 
+            },
+            "14": {
+                "color": "red",
+                "parity": "even" 
+            },
+            "2": {
+                "color": "black",
+                "parity": "even" 
+            } 
+        }
+    };
+
+    var tests = {
+        // TODO: add "00" tests and move server-side
+        column_test: function (result) {
+            return layout_config.columns.column_1.indexOf(Number(result)) !== -1 ? 'column_1' : layout_config.columns.column_2.indexOf(Number(result))!== -1 ? 'column_2' : 'column_3';
+        },
+        which_third_test: function (result) {
+            return Number(result) < 13 ? '1_12' : Number(result) < 25 ? '13_24' : '25_36';        
+        },
+        which_half_test: function (result) {
+            return Number(result) < 19 ? '1_18' : '19_36'    
+        },
+        parity_test: function (result) {
+            return result === 'even' ? true : result === 'odd' ? true : 'something went wrong...';
+        }
+    }
+
+    function flash() {
+        document.getElementById('flash').innerHTML = 'We have a winner!';
+    }
+    
+    var clear = {
+        flash: function () {
+            document.getElementById('flash').innerHTML = '';
+        },
+        bets: function () {
+            $('#layout').find('.chip').removeClass('chip');    
+        },
+        results: function () {
+            $('#layout').find('.result').removeClass('result');
+        }    
+    };
+
+    var chips = {
+        owner: null,
+        chip_count: 5,
+        credit: function (chips) {
+            var credit_amount = chips || 1;
+            this.chip_count += credit_amount;
+            this.update_chip_count();
+            socket.send(this);
+        },
+        debit: function (chips) {
+            var debit_amount = chips || 1;
+            this.chip_count -= debit_amount;
+            this.update_chip_count();
+            socket.send(this);
+        },
+        update_chip_count: function () {
+            if (chips.chip_count) {
+                document.getElementById('chip_count').innerHTML = chips.chip_count;
+            } else {
+                document.getElementById('chip_count').innerHTML = 'ca$hed out!';            
+            }
+        }
+    };
+
+    (function () {
+        var chip_count = document.createElement('li');
+        chip_count.id = 'chip_count';        
+        chip_count.innerHTML = chips.chip_count;
+        document.getElementById('chips').appendChild(chip_count);
+    }());
+
+    document.getElementById('buy_more_chips').onclick = function () {
+        chips.credit(5);
+    };
+
+    var socket = new io.Socket(null, {port: 8000});
+
+    $('#layout').delegate('td', 'click', function () {
+        if ($(this).hasClass('chip')) {
+            chips.credit();
+            $(this).removeClass('chip');
+        } else if (chips.chip_count) {
+            chips.debit();                
+            $(this).addClass('chip');
+        } else {
+alert('Please take a left at the Native American Motif behind the table and purchase some additional chips.');        
+        }
+    });
+
+    socket.connect().on('message', function (msg) {
+        if (msg.players_arr) {
+            document.getElementById('players').innerHTML = msg.players_arr;
+        }
+        if (msg.spin) {
+            var result = msg.spin;
+            clear.flash();
+            $('.result').removeClass('result');
+            $('#wheel').addClass('spin');
+            setTimeout(function () {
+                var li_result = document.createElement('li')
+                    ,   bets = []
+                    ,   column = tests.column_test(result.number)
+                    ,   which_third = tests.which_third_test(result.number)                
+                    ,   which_half = tests.which_half_test(result.number)                
+                    ,   winner = false
+                    ,   bet
+                    ,   l
+                    ,   $el
+                ;
+                li_result.innerHTML = result.number + ' ' + result.color + ' ' + result.parity;
+                document.getElementById('results').appendChild(li_result);
+
+//                $('#layout').find('.chip').each(function () {
+//                    bets[bets.length] = this.id;
+//                });
+
+//                for (bet = 0, l = bets.length; bet < l; bet += 1) {
+//                    $el = $(document.getElementById(bets[bet]));
+//                    if ($el.hasClass('color')) {
+//                        if (bets[bet] === result['color']) {
+//                            winner = true;
+//                            chips.credit(1);
+//                        }
+//                    }
+//                    if ($el.hasClass('half')) {
+//                        if (bets[bet] === which_half) {
+//                            winner = true;
+//                            chips.credit(1);
+//                        }
+//                    }
+//                    if ($el.hasClass('third')) {
+//                        if (bets[bet] === which_third) {
+//                            winner = true;
+//                            chips.credit(3);
+//                        }
+//                    }                
+//                    if (tests.parity_test(bets[bet])) {
+//                        if (bets[bet] === result['parity']) {
+//                            winner = true;
+//                            chips.credit(2);
+//                        }
+//                    }
+//                    if (bets[bet] === result['number']) {
+//                        winner = true;
+//                        chips.credit(35);
+//                    }
+//                }
+        
+                if (winner) {
+                    flash();
+                }
+
+                clear.bets();            
+
+                $('#' + result.number + ', #' + result.parity + ', #' + result.color + ', #' + which_half + ', #' + which_third + ', #' + column).addClass('result');
+
+                setTimeout(function () {
+                    $('#wheel').removeClass('spin');
+                    clear.results();
+                }, 4000);
+            }, 2000);
+        }
+    });
+
+    var user_name = prompt('hey there! what\'s your name?');
+    if (user_name) {
+        var user = {
+            user_name: user_name 
+        };
+console.log(user);
+        socket.send(user);        
+        chips.owner = user_name;
+        document.getElementsByTagName('body')[0].className = '';
+    }
+        
+}());
